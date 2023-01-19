71e9cefdb45f8f3b506e2d8b12f69e06d2bee7b2
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-01-19 15:30:31 -0700

    Working version with forms that submit

--- habits/forms.py
+++ habits/forms.py
@@ -1,10 +1,26 @@
 from django import forms
+from habits.models import Habit, HabitResponse
 
-class ResponseForm(forms.Form):
-    response = forms.CharField(
-        max_length=1,
-        widget=forms.TextInput(attrs={
-            "class": "form-control",
-            "placeholder": "Your Name"
-        })
+class DayTrackerForm(forms.Form):
+    response_id = forms.CharField(
+        widget=forms.HiddenInput()
+        # initial = HabitResponse.id
     )
+    response_char = forms.CharField(
+        widget=forms.HiddenInput()
+        # initial = HabitResponse.id
+    )
+
+class HabitResponseForm(forms.Form):
+    response_char = forms.CharField(max_length=1) # G - Green // N - Neither // R - Red
+    #report_day = models.DateTimeField()
+
+'''
+<form method="post" >
+    <input type="hidden" value = "{{ habitResponse.id }}" />
+    <button class = "response" id="{{ habitResponse.id }}" type="submit"> 
+        <!--onclick="cycleColor('{{ habitResponse.id }}', '{{habitResponse.response_char}}')"-->
+        {{ habitResponse.response_char }}
+    </button>
+</form>
+'''
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from habits.models import Habit, HabitResponse
-from .forms import ResponseForm
+from .forms import DayTrackerForm, HabitResponseForm
 
 # Create your views here.
 def habit_index(request):
@@ -13,20 +13,34 @@ def habit_index(request):
 def habit_detail(request, pk):
     habit = Habit.objects.get(pk=pk)
 
-    form = ResponseForm()
     if request.method == 'POST':
-        form = ResponseForm(request.POST)
+        form = DayTrackerForm(request.POST)
+        #Todo: hide the hidden field on the form // FIX CSS styling on form // 
+        #Todo: update color styling based on response char
+        #Todo: provide the default value for the response_id in the form. (populate the hidden form for the user)
+        #? get vs post in line 16 ?
+        #? learn how to make a branch and start saving to it (consider saving things straight to the main branch)?
+        #Cycles through characters and updates database
         if form.is_valid():
-            response = ResponseForm(
-                response=form.cleaned_data["response"],
-                habit=habit
-            )
-            response.save()
+            # N - Neither // G - Green // R - Red
+            hr = HabitResponse.objects.get(id=form.cleaned_data['response_id'])
+            if hr.response_char == 'N':
+                hr.response_char = 'G'
+            elif hr.response_char == 'G':
+                hr.response_char = 'R'
+            elif hr.response_char == 'R':
+                hr.response_char = 'N'
+            else: 
+                print("not an accepted character")
+            hr.save()
 
     habitResponses = HabitResponse.objects.filter(habit=habit)
+    forms = [DayTrackerForm(initial={'response_id': hr.id, 'response_char': hr.response_char}) for hr in habitResponses]
+
     context = {
         'habit': habit,
-        'habitResponses' : habitResponses,
+        # 'habitResponses' : habitResponses,
+        'forms' : forms
     }
 
     return render(request, 'habit_detail.html', context)
